1d2d00d8800ddc5266b7f88b65ac7f0749f3e021
Mason <masonlynass@gmail.com>
2025-02-28 20:26:01 -0800

    testing adding a new document type to Sanity

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -6,6 +6,7 @@ import author from "./schemas/documents/author";
 import category from "./schemas/documents/category";
 import faq from "./schemas/documents/faq";
 import testimonial from "./schemas/documents/testimonial";
+import testform from "./schemas/documents/testform"
 
 // Schema UI shared objects
 import blockContent from "./schemas/blocks/shared/block-content";
@@ -47,6 +48,7 @@ export const schema: { types: SchemaTypeDefinition[] } = {
     category,
     faq,
     testimonial,
+    testform,
     // shared objects
     blockContent,
     link,
