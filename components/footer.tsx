b6978a4e4b15fefdd5d7d3b2d0f92a7c47834e15
Mason <masonlynass@gmail.com>
2025-03-03 17:16:24 -0800

    changes to footer components & styling, a bit of header styling

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,11 @@
-'use client'
+"use client";
 import Link from "next/link";
 import Logo from "@/components/logo";
 import { useState } from "react";
 import ContactDialog from "./custom/contactdialog";
 import { Button } from "./ui/button";
+import clsx from "clsx";
+import SocialIcons from "./custom/socialicons";
 
 const navItems = [
   {
@@ -29,64 +31,76 @@ const navItems = [
 ];
 
 export default function Footer() {
+  console.log("something");
 
-  console.log('something')
-
-  const [contactDialogOpen, setContactDialogOpen] = useState(false)
+  const [contactDialogOpen, setContactDialogOpen] = useState(false);
 
   const getCurrentYear = () => {
     return new Date().getFullYear();
   };
 
   return (
-    <div>
+    <div className="relative z-10">
       {/* maybe this div should be its own component */}
-      <div className={`flex flex-col left-10 bottom-20 fixed`}>
-        <p>B</p>
-        <p>I</p>
-        <p>F</p>
-        <p>Y</p>
-      </div>
-      <Button className={`fixed right-10 bottom-20 bg`} onClick={() => setContactDialogOpen(!contactDialogOpen)}>Let's Chat</Button>
-      <ContactDialog contactDialogOpen={contactDialogOpen} setContactDialogOpen={setContactDialogOpen}/>
-    <footer>
-      <div className="dark:bg-background pb-5 xl:pb-5 dark:text-gray-300">
-        <Link
-          className="block w-[10rem] mx-auto"
-          href="/"
-          aria-label="Home page"
+      <div className="relative z-10">
+        <SocialIcons
+          className={`flex flex-col left-5 bottom-10 gap-2 fixed`}
+        />
+        {/* <p className={`fixed right-1/36 bottom-20 bg-gray-500 z-[5]`}>Hello!</p> */}
+        <Button
+          className={`fixed right-5 bottom-10 z-[5]`}
+          onClick={() => setContactDialogOpen(!contactDialogOpen)}
         >
-          <Logo />
-        </Link>
-        <div className="mt-8 flex flex-wrap items-center justify-center gap-7 text-primary">
-          {navItems.map((navItem) => (
-            <Link
-              key={navItem.label}
-              href={navItem.href}
-              target={navItem.target ? "_blank" : undefined}
-              rel={navItem.target ? "noopener noreferrer" : undefined}
-              className="transition-colors hover:text-foreground/80 text-foreground/60 text-sm"
-            >
-              {navItem.label}
-            </Link>
-          ))}
-          <p
-            key="Contact Us"
-            className={
-              "transition-colors hover:text-foreground/80 text-foreground/60 text-sm"
-            }
-            onClick={() => setContactDialogOpen}
-          >
-            Contact Us
-          </p>
-        </div>
-        <div className="mt-8 flex flex-col lg:flex-row gap-6 justify-center text-center lg:mt-5 text-xs border-t pt-8">
-          <p className="text-foreground/60">
-            &copy; {getCurrentYear()} Built by Infinite Spaces
-          </p>
-        </div>
+          Contact Us
+        </Button>
+
+        <ContactDialog
+          contactDialogOpen={contactDialogOpen}
+          setContactDialogOpen={setContactDialogOpen}
+        />
       </div>
-    </footer>
+      <footer className="relative z-[100] bg-white">
+        <div className="relative dark:bg-background py-5 xl:py-5 dark:text-gray-300 z-[100]">
+          <Link
+            className="block w-fit mx-auto"
+            href="/"
+            aria-label="Home page"
+          >
+            <Logo />
+          </Link>
+          <div className="pt-15 flex flex-row justify-between items-center mx-8">
+            <SocialIcons className={'flex-row'}/>
+            <div className=" flex flex-wrap items-center justify-center gap-7 text-primary z-50">
+              {navItems.map((navItem) => (
+                <Link
+                  key={navItem.label}
+                  href={navItem.href}
+                  target={navItem.target ? "_blank" : undefined}
+                  rel={navItem.target ? "noopener noreferrer" : undefined}
+                  className="transition-colors hover:text-foreground/80 text-foreground/60 text-sm"
+                >
+                  {navItem.label}
+                </Link>
+              ))}
+            </div>
+            <Button
+              key="Contact Us"
+              className={
+                "transition-colors text-white right-5 bottom-20 z-[5]"
+              }
+              onClick={() => setContactDialogOpen}
+            >
+              Contact Us
+            </Button>
+          </div>
+
+          <div className="mt-8 flex flex-col lg:flex-row gap-6 justify-center text-center lg:mt-5 text-xs border-t pt-8">
+            <p className="text-foreground/60">
+              &copy; {getCurrentYear()} Built by Infinite Spaces
+            </p>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 }
