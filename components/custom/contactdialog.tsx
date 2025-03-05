0c0c59cf4dd97d6f623d81ffbf12c47480e72911
Mason <masonlynass@gmail.com>
2025-03-04 17:57:04 -0800

    small contact us updates

--- components/custom/contactdialog.tsx
+++ components/custom/contactdialog.tsx
@@ -1,7 +1,10 @@
+import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Form, FormDescription } from "../ui/form";
 import Newsletter from "@/sanity/schemas/blocks/forms/newsletter";
 import FormNewsletter from "../ui/forms/newsletter";
+import { Button } from "../ui/button";
+import { motion } from "motion/react";
 
 interface ContactDialogProps {
   contactDialogOpen: boolean;
@@ -14,22 +17,51 @@ export default function ContactDialog({
 }: ContactDialogProps) {
   const form = useForm();
 
-  console.log(contactDialogOpen)
+  const [isExiting, setIsExiting] = useState(false);
+
+  console.log(contactDialogOpen);
+
+  function handleClose () {
+    setIsExiting(true)
+    setTimeout(() => {
+      setIsExiting(false)
+      setContactDialogOpen(false)
+    }, 500);
+  }
 
   return (
     // eventually something with a separate handleSubmit function like this:
     // https://schemaui.com/docs/components/forms/form-newsletter
-    <dialog className={`fixed bg-gray-900 w-full top-0 right-0 h-full ${contactDialogOpen ? "flex flex-row w-100 z-[200]" : "hidden"}`}>
-      {/* <form onSubmit={() => form.handleSubmit(onSubmit)}></form> */}
-      <div>
-        <h1>Contact Us</h1>
-        <p>more text about contacting us</p>
-        <p onClick={() => setContactDialogOpen(false)}>CLOSE ME</p>
-      </div>
-      <Form {...form}>
+    contactDialogOpen && (
+      <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        exit={{ opacity: 0, y: 100 }}
+        transition={{duration: 0.5}}
+        className={` bg-gray-900 text-white  ${contactDialogOpen ? "fixed h-full w-full top-0 left-0 z-9999" : "hidden"}`}
+      >
+        <Button
+          className={
+            "absolute right-5 top-20 bg-white text-black hover:bg-gray-200"
+          }
+          onClick={() => handleClose()}
+          disabled={isExiting}
+        >
+          X
+        </Button>
+        {/* <form onSubmit={() => form.handleSubmit(onSubmit)}></form> */}
+        <div
+          className={`flex flex-col justify-center items-center mx-auto my-30 px-0 w-fit`}
+        >
+          <h1>Contact Us</h1>
+          <p>more text about contacting us</p>
+        </div>
+
+        {/* <Form {...form}>
         <form onSubmit={() => console.log("submitted that form")}></form>
         <FormDescription>something</FormDescription>
-      </Form>
-    </dialog>
+      </Form> */}
+      </motion.div>
+    )
   );
 }
