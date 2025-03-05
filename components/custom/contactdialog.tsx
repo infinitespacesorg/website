40f39fd63a49e0a68ebe29c81ecc31dbbc205336
Mason <masonlynass@gmail.com>
2025-03-04 17:17:48 -0800

    small schema change, mostly styling changes

--- components/custom/contactdialog.tsx
+++ components/custom/contactdialog.tsx
@@ -14,14 +14,17 @@ export default function ContactDialog({
 }: ContactDialogProps) {
   const form = useForm();
 
+  console.log(contactDialogOpen)
+
   return (
     // eventually something with a separate handleSubmit function like this:
     // https://schemaui.com/docs/components/forms/form-newsletter
-    <dialog className={contactDialogOpen ? "flex flex-row" : "hidden"}>
+    <dialog className={`fixed bg-gray-900 w-full top-0 right-0 h-full ${contactDialogOpen ? "flex flex-row w-100 z-[200]" : "hidden"}`}>
       {/* <form onSubmit={() => form.handleSubmit(onSubmit)}></form> */}
       <div>
         <h1>Contact Us</h1>
         <p>more text about contacting us</p>
+        <p onClick={() => setContactDialogOpen(false)}>CLOSE ME</p>
       </div>
       <Form {...form}>
         <form onSubmit={() => console.log("submitted that form")}></form>
