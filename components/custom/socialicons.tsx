41d9d5029faba62726dbe651b30218972d1ea129
Steve Judkins <steve@infinitespaces.org>
2025-03-05 16:12:04 -0800

    Remove sample data, update readme, and social icons

--- components/custom/socialicons.tsx
+++ components/custom/socialicons.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faFacebook, faInstagram, faYoutube, faBluesky } from "@fortawesome/free-brands-svg-icons";
 
@@ -5,6 +6,56 @@ interface SocialIconsProps {
     className: string;
 }
 
+const navItems = [
+    {
+      label: "Bluesky",
+      icon: faBluesky,
+      class: "w-10 h-10 text-gray-700 dark:text-gray-300 hover:text-blue-500",
+      href: "https://bsky.app/profile/infinitespaces.bsky.social",
+      target: true,
+    },
+    {
+      label: "Instagram",
+      icon: faInstagram,
+      class: "w-10 h-10 text-gray-700 dark:text-gray-300 hover:text-pink-500",
+      href: "https://www.instagram.com/infinite.spaces/",
+      target: true,
+    },
+    {
+      label: "Facebook",
+      icon: faFacebook,
+      class: "w-10 h-10 text-gray-700 dark:text-gray-300 hover:text-red-500",
+      href: "https://www.facebook.com/InfiniteSpacesCreatives",
+      target: true,
+    },
+    {
+      label: "Youtube",
+      icon: faYoutube,
+      class: "w-10 h-10 text-gray-700 dark:text-gray-300 hover:text-blue-700",
+      href: "https://www.youtube.com/@infinite.spaces",
+      target: true,
+    },
+];
+
+
+export default function SocialIcons ({className = ''}: SocialIconsProps) {
+    return (
+        <div className={className}>
+            {navItems.map((navItem) => (
+                <Link
+                key={navItem.label}
+                href={navItem.href}
+                target={navItem.target ? "_blank" : undefined}
+                rel={navItem.target ? "noopener noreferrer" : undefined}
+                >
+                    <FontAwesomeIcon icon={navItem.icon} className={navItem.class} />
+                </Link>
+            ))}
+        </div>
+    )
+}
+
+/*
 export default function SocialIcons ({className = ''}: SocialIconsProps) {
     return (
         <div className={className}>
@@ -15,3 +66,4 @@ export default function SocialIcons ({className = ''}: SocialIconsProps) {
         </div>
     )
 }
+*/
