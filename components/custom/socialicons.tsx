4ddbb5e977b22e609d862c7b8024c9d93c792459
Mason <masonlynass@gmail.com>
2025-03-07 10:39:12 -0800

    social icons sizing, header nav dark mode

--- components/custom/socialicons.tsx
+++ components/custom/socialicons.tsx
@@ -10,28 +10,28 @@ const navItems = [
     {
       label: "Bluesky",
       icon: faBluesky,
-      class: "w-10 h-10 text-gray-700 dark:text-gray-300 hover:text-blue-500",
+      class: "hover:text-blue-500",
       href: "https://bsky.app/profile/infinitespaces.bsky.social",
       target: true,
     },
     {
       label: "Instagram",
       icon: faInstagram,
-      class: "w-10 h-10 text-gray-700 dark:text-gray-300 hover:text-pink-500",
+      class: "hover:text-pink-500",
       href: "https://www.instagram.com/infinite.spaces/",
       target: true,
     },
     {
       label: "Facebook",
       icon: faFacebook,
-      class: "w-10 h-10 text-gray-700 dark:text-gray-300 hover:text-red-500",
+      class: "hover:text-red-500",
       href: "https://www.facebook.com/InfiniteSpacesCreatives",
       target: true,
     },
     {
       label: "Youtube",
       icon: faYoutube,
-      class: "w-10 h-10 text-gray-700 dark:text-gray-300 hover:text-blue-700",
+      class: "hover:text-blue-700",
       href: "https://www.youtube.com/@infinite.spaces",
       target: true,
     },
@@ -48,7 +48,7 @@ export default function SocialIcons ({className = ''}: SocialIconsProps) {
                 target={navItem.target ? "_blank" : undefined}
                 rel={navItem.target ? "noopener noreferrer" : undefined}
                 >
-                    <FontAwesomeIcon icon={navItem.icon} className={navItem.class} />
+                    <FontAwesomeIcon icon={navItem.icon} className={`${navItem.class} w-4 h-4 text-gray-700 dark:text-gray-300`} />
                 </Link>
             ))}
         </div>
