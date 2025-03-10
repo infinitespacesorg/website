85bbed599d3c560fb19d71fa0aca6db3524b2318
Mason <masonlynass@gmail.com>
2025-03-10 12:25:52 -0700

    let's try this

--- app/api/submitAYTForm/route.ts
+++ app/api/submitAYTForm/route.ts
@@ -4,8 +4,8 @@ const client = createClient({
     projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
     dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
     useCdn: false,
-    apiVersion: process.env.SANITY_API_VERSION,
-    token: process.env.SANITY_API_TOKEN,
+    apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
+    token: process.env.SANITY_AUTH_TOKEN,
 });
 
 interface FormSubmission {
