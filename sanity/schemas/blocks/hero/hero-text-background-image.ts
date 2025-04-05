a1f7fc466e787d22511ec190ace690ad02cd446b
Mason <masonlynass@gmail.com>
2025-04-04 20:07:19 -0700

    hero-text-background-image has a size option now

--- sanity/schemas/blocks/hero/hero-text-background-image.ts
+++ sanity/schemas/blocks/hero/hero-text-background-image.ts
@@ -1,6 +1,19 @@
 import { defineField, defineType } from "sanity";
 import { TextSelect } from "lucide-react";
 
+const SIZE_VARIANTS = [
+  {title: '1', value: 'text-lg'},
+  {title: '2', value: 'text-xl'},
+  {title: '3', value: 'text-2xl'},
+  {title: '4', value: 'text-3xl'},
+  {title: '5', value: 'text-4xl'},
+  {title: '6', value: 'text-5xl'},
+  {title: '7', value: 'text-6xl'},
+  {title: '8', value: 'text-7xl'},
+  {title: '9', value: 'text-8xl'},
+  {title: '10', value: 'text-9xl'},
+]
+
 export default defineType({
   name: "hero-text-background-image",
   title: "Hero Text Background Image",
@@ -25,6 +38,15 @@ export default defineType({
         },
       ],
     }),
+    defineField({
+      name: 'size',
+      type: 'string',
+      options: {
+        list: SIZE_VARIANTS.map(({ title, value }) => ({ title, value })),
+        layout: "radio",
+      },
+      initialValue: "6",
+    })
   ],
   preview: {
     select: {
