500f7630df170399f3e062dc972fb5a29f825aaa
Mason <masonlynass@gmail.com>
2025-04-09 22:10:03 -0700

    gotta catch em all

--- app/(main)/auth/sync/page.tsx
+++ app/(main)/auth/sync/page.tsx
@@ -1,18 +1,27 @@
 "use client";
 
-import { useEffect } from "react";
+import { Suspense, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 
-export default function AuthSync() {
+function AuthSyncHandler () {
   const next = useSearchParams().get("next") || "/";
 
   useEffect(() => {
     window.location.replace(next);
   }, [ next]);
 
+  return null
+}
+
+export default function AuthSync() {
+  
+
   return (
     <main className="flex items-center justify-center h-screen">
       <p className="text-muted-foreground text-sm">Working...</p>
+      <Suspense>
+        <AuthSyncHandler />
+      </Suspense>
     </main>
   );
 }
