9987bb8dc0f723992205c496397c85425027609a
Mason <masonlynass@gmail.com>
2025-04-09 22:04:25 -0700

    let's try it this way

--- app/(main)/(auth-pages)/login/page.tsx
+++ app/(main)/(auth-pages)/login/page.tsx
@@ -7,10 +7,21 @@ import { Suspense, useEffect } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 import { parseMessageFromSearchParams } from "@/lib/utils";
 
-export default function Login() {
-  const { authUser, account, loading } = useUser();
-  const router = useRouter();
+function LoginView() {
   const searchParams = useSearchParams();
+  const rawMessage = parseMessageFromSearchParams(searchParams);
+  const message = rawMessage ?? null;
+  const view = searchParams.get("view") || "signin";
+
+  if (view === "forgot") return <ForgotPassword message={message} />;
+  if (view === "signup") return <SignUp message={message} />;
+  return <SignIn message={message} />;
+}
+
+export default function Login() {
+  const { authUser, loading } = useUser();
+  const router = useRouter();
+  
 
   // if there's a user, redirect to /account
   useEffect(() => {
@@ -21,21 +32,13 @@ export default function Login() {
 
   if (loading || authUser) return null;
 
-  const rawMessage = parseMessageFromSearchParams(searchParams);
-  const message = rawMessage ?? null;
-  const view = searchParams.get("view") || "signin";
+  
 
   // if there isn't a user, then show the sign in page
   return (
     <main className="w-80 m-auto py-16">
       <Suspense>
-        {view === "forgot" ? (
-          <ForgotPassword message={message} />
-        ) : view === "signup" ? (
-          <SignUp message={message} />
-        ) : (
-          <SignIn message={message} />
-        )}
+        <LoginView/>
       </Suspense>
     </main>
   );
