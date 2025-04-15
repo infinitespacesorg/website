92a9368ad058060a6df6743c22ff8d7d612ae91e
Mason <masonlynass@gmail.com>
2025-04-15 14:53:22 -0700

    updated nav to handle JoinWaitlist

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,5 @@
-import Header from "@/components/header";
-import Footer from "@/components/footer";
+import Header from "@/components/header/index-nav";
+import Footer from "@/components/footer/footer";
 import Custom404 from "@/components/404";
 
 import type { Metadata } from "next";
