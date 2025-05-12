43a67d85f54d1151563b9b10ac9a4bd9e9a0c0c5
Mason <masonlynass@gmail.com>
2025-05-12 16:49:59 -0700

    styling

--- app/(main)/(protected)/account/layout.tsx
+++ app/(main)/(protected)/account/layout.tsx
@@ -2,7 +2,7 @@ import Sidebar from './Sidebar'
 
 export default function AccountLayout({ children}: { children: React.ReactNode}) {
     return (
-        <div className='flex flex-col md:flex-row items-center justify-start min-h-[60vh] md:min-h-[85vh]'>
+        <div className='flex flex-col md:flex-row items-center md:items-start justify-start min-h-[60vh] md:min-h-[85vh]'>
             <Sidebar />
             <div className='flex-1 p-3 md:p-6 md:py-15'>
                 {children}
