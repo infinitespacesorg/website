56ca75ff0c6d111fb11bade557b59a9b33bbfba2
Mason <masonlynass@gmail.com>
2025-05-12 13:21:10 -0700

    another try

--- app/(main)/about/page.tsx
+++ app/(main)/about/page.tsx
@@ -1,16 +1,11 @@
+export const dynamic = "force-dynamic";
+
 import Blocks from "@/components/blocks";
 import { fetchSanityPageBySlug } from "../actions";
 import { generatePageMetadata } from "@/lib/metadata";
 import MissingSanityPage from "@/components/ui/missing-sanity-page";
-import dynamic from "next/dynamic";
-
-const ElseHeader = dynamic(() => import("@/components/header/else-nav"), {
-  ssr: false,
-});
-
-const FooterWithContactUs = dynamic(() => import("@/components/footer/footer-with-contact-us"), {
-  ssr: false,
-});
+import FooterWithContactUs from "@/components/footer/footer-with-contact-us";
+import ElseHeader from "@/components/header/else-nav";
 
 export async function generateMetadata() {
   const page = await fetchSanityPageBySlug({ slug: "about" });
