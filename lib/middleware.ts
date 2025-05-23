ee42e5675528bf4c2e11947065db83f1d2f61883
Steve Judkins <steve@infinitespaces.org>
2025-05-22 18:00:41 -0700

    Need to use a different env key for S3 bucket writes since Mason is using these for Supabase writes

--- lib/middleware.ts
+++ lib/middleware.ts
@@ -7,8 +7,8 @@ export async function updateSession(request: NextRequest) {
   })
 
   const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL_S3!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY_S3!,
     {
       cookies: {
         getAll() {
--- lib/server.ts
+++ lib/server.ts
@@ -5,8 +5,8 @@ export async function createClient() {
   const cookieStore = await cookies()
 
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_URL_S3!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY_S3!,
     {
       cookies: {
         getAll() {
