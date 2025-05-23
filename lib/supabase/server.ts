c84e7ec256cbf6dcfbbb6959af3abb602d6202c4
Steve Judkins <steve@infinitespaces.org>
2025-05-22 17:44:45 -0700

    Password Protected

---
+++ lib/supabase/server.ts
@@ -0,0 +1,38 @@
+// things related to Supabase
+// added by Mason, taken from https://github.com/vercel/next.js/tree/canary/examples/with-supabase/utils/supabase
+
+// THIS CURRENTLY WORKS PLEASE DO NOT TRY TO CHANGE IT UNTIL @supabase/ssr 0.7 or 0.6.2 at least
+
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from 'next/headers'
+
+export async function createSupabaseServerClient() {
+
+  const cookieStore = await cookies()
+
+  return createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      auth: {
+        flowType: "pkce"
+      },
+      cookies: {
+        getAll() {
+          return cookieStore.getAll()
+        },
+        setAll(cookiesToSet) {
+          try {
+            cookiesToSet.forEach(({ name, value, options }) =>
+              cookieStore.set(name, value, options)
+            )
+          } catch {
+            // The `setAll` method was called from a Server Component.
+            // This can be ignored if you have middleware refreshing
+            // user sessions.
+          }
+        },
+      },
+    }
+  )
+}
