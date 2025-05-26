74887d750e0e0557aeda1bc809a245f4a455084a
Mason <masonlynass@gmail.com>
2025-05-26 13:15:40 -0700

    new block for standalone block-content

---
+++ sanity/schemas/blocks/basic-block-content.ts
@@ -0,0 +1,26 @@
+import { defineField, defineType } from "sanity";
+import { TextQuote } from "lucide-react";
+
+export default defineType({
+  name: "basic-block-content",
+  type: "object",
+  icon: TextQuote,
+  title: "Basic Block Content",
+  description: "A basic block content element to add various content.",
+  fields: [
+    defineField({
+      name: "padding",
+      type: "section-padding",
+    }),
+    defineField({
+      name: "customTailwind",
+      title: 'Custom Tailwind',
+      type: "string",
+      description: 'add your own custom Tailwind here to modify the styling of this block'
+    }),
+    defineField({
+      name: "body",
+      type: "block-content",
+    }),
+  ]
+});
