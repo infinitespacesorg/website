3803ba94a8a29ef989e09dde62a3495ca2be70f6
Mason <masonlynass@gmail.com>
2025-06-16 15:51:36 -0700

    update redirect_to

--- components/header/else-nav/index.tsx
+++ components/header/else-nav/index.tsx
@@ -15,7 +15,7 @@ export default function ElseHeader() {
     { label: "Home", href: "/", target: false },
     // { label: "Blog", href: "/blog", target: false },
     { label: "About", href: "/about", target: false },
-    { label: "Login", href: "https://playbox.infinitespaces.co/auth/login", target: false },
+    { label: "Login", href: "https://playbox.infinitespaces.co/auth/login?redirect_to=https://infinitespaces.co/account/profile", target: false },
   ]);
 
   const { authUser } = useUser();
