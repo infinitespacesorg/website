08b9720849c30fe507313267dc1343ada858770a
Mason Lynass <masonlynass@gmail.com>
2025-06-17 13:15:53 -0700

    changed some supabase config, will this help?

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -13,8 +13,8 @@ export async function createSupabaseServerClient() {
   const cookieStore = await cookies()
 
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_WEBSITE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_WEBSITE_ANON_KEY!,
     {
       auth: {
         flowType: "pkce"
