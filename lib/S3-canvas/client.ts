2e00d0a3ae4a3806a559a3857acc5c1727a58be7
Mason Lynass <masonlynass@gmail.com>
2025-07-01 15:48:32 -0700

    trying to recognize the user!

--- lib/S3-canvas/client.ts
+++ lib/S3-canvas/client.ts
@@ -4,5 +4,9 @@ export function createClient() {
   return createBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_PLAYBOX_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_PLAYBOX_ANON_KEY!,
+    {auth: {
+      persistSession: true,
+      autoRefreshToken: true
+    }}
   )
 }
