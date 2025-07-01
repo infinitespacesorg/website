824345a380973e3346511ef681c3ac8d4277e643
Mason Lynass <masonlynass@gmail.com>
2025-07-01 15:19:05 -0700

    another thing

--- app/(main)/auth/sync/route.ts
+++ app/(main)/auth/sync/route.ts
@@ -11,6 +11,8 @@ export async function GET(request: Request) {
     data: { user },
   } = await supabase.auth.getUser();
 
+  console.log('/sync user', user)
+
   if (user) {
     return redirect(next);
   }
