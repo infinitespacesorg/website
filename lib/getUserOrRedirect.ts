763b45a229895f80637f3d6b386e0a169cdbd692
Mason Lynass <masonlynass@gmail.com>
2025-07-01 10:24:16 -0700

    is this the issue

--- lib/getUserOrRedirect.ts
+++ lib/getUserOrRedirect.ts
@@ -1,19 +1,15 @@
 import { redirect } from "next/navigation";
 import { createClient } from "./S3-canvas/server";
-import { useUser } from "@/context/UserContext";
 
 export async function getUserOrRedirect() {
       
     const supabase = await createClient()
-    const { refreshUserContext } = useUser()
 
     const {
         data: { user },
     } = await supabase.auth.getUser();
 
     if (!user) redirect('/login')
-
-    refreshUserContext()
 
     return user
 }
